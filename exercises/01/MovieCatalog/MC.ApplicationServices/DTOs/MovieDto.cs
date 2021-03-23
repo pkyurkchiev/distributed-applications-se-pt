258c4f0706494b1d689b0d7fc544c19e428f8dc9
Pavel Kyurkchiev <pavkyu@gmail.com>
2021-03-23 12:17:30 +0200

    day 4 exercise

---
+++ exercises/01/MovieCatalog/MC.ApplicationServices/DTOs/MovieDto.cs
@@ -0,0 +1,17 @@
+﻿using MC.Data.Entities;
+
+namespace MC.ApplicationServices.DTOs
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public MovieDto() { }
+        public MovieDto(Movie movie)
+        {
+            Id = movie.Id;
+            Title = movie.Title;
+        }
+    }
+}
