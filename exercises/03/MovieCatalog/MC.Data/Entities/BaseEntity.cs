d1c9a1e7122792345f2450d33a77feae3f078482
Pavel Kyurkchiev <pavkyu@gmail.com>
2021-03-25 09:33:16 +0200

    exercices 3

---
+++ exercises/03/MovieCatalog/MC.Data/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MC.Data.Entities
+{
+    public class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ exercises/03/MovieCatalog/MC.Website/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MC.Website.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MoviesServiceReference.MoviesClient moviesClient = new MoviesServiceReference.MoviesClient();
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = moviesClient.GetMovies();
+            return View(movies);
+        }
+    }
+}
