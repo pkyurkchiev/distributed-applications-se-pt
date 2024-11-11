61ef8f6c1af9ede35bf147b728d3f292a4bd31be
Mustafa Buhov <118231484+MustyX23@users.noreply.github.com>
2024-11-11 22:52:13 +0200

    Data/code first approach (#5)
    * Add EF Core as dependency
    
    * Configuring DbContext
    
    * Added Models folder

---
+++ GustoHub.API/GustoHub.Data/GustoHubDbContext.cs
@@ -0,0 +1,30 @@
+﻿namespace GustoHub.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class GustoHubDbContext : DbContext
+    {
+        /*
+         * The constructor takes a parameter of type DbContextOptions<GustoHubDbContext>,
+         * which configures the context with specific options (like connection strings and database providers).
+         * The base constructor is called with these options to initialize the context properly.
+         */
+        public GustoHubDbContext(DbContextOptions<GustoHubDbContext> options)
+            :base(options)
+        {           
+        }
+
+        //TODO:
+        //Add DbSet...
+
+        /*
+         * The OnModelCreating method is overridden to configure the model and relationships using Fluent API.
+         * Calling base.OnModelCreating(modelBuilder) ensures that any configurations
+         * set up in the base class are applied.
+         */
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
