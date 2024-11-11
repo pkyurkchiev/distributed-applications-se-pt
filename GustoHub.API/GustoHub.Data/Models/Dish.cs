71ca0b2ec35986a18f400182c3ed334335ad4348
Mustafa Buhov <mustyx900@gmail.com>
2024-11-11 23:14:45 +0200

    Added Models

---
+++ GustoHub.API/GustoHub.Data/Models/Dish.cs
@@ -0,0 +1,22 @@
+﻿namespace GustoHub.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Dish
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty!;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; } = null!;
+
+        public ICollection<OrderDish> OrderDishes { get; set; } = new List<OrderDish>();
+    }
+}
