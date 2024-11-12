4b72e2fe0c1048e918480d123c1c8333f093d4fd
Mustafa Buhov <118231484+MustyX23@users.noreply.github.com>
2024-11-12 23:53:36 +0200

    Create CategoriesController.cs (#13)
    Only one action is set, more to come

---
+++ GustoHub.API/GustoHub.API/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿namespace GustoHub.API.Controllers
+{
+    using GustoHub.Data.Models;
+    using GustoHub.Services.Interfaces;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Category>> PostCategory([FromBody] Category category)
+        {
+            await categoryService.AddCategoryAsync(category);
+            return Ok(category);
+        }
+    }
+}
