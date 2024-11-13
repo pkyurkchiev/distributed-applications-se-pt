6ff51be5f01b020480b38799673550ed86cbfff6
Mustafa Buhov <mustyx900@gmail.com>
2024-11-13 22:00:28 +0200

    Implement and Initialize CategoryService base functionality

--- GustoHub.API/GustoHub.Services/Services/CategoryService.cs
+++ GustoHub.API/GustoHub.Services/Services/CategoryService.cs
@@ -3,6 +3,8 @@
     using GustoHub.Data.Common;
     using GustoHub.Data.Models;
     using GustoHub.Services.Interfaces;
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     public class CategoryService : ICategoryService
@@ -14,12 +16,48 @@
             this.repository = repository;
         }
 
-        public async Task<int> AddCategoryAsync(Category category)
+        public async Task<string> AddAsync(Category category)
         {
-            await repository.AddAsync(category);
-            await repository.SaveChangesAsync();
+            if (!await ExistsByIdAsync(category.Id))
+            {
+                await repository.AddAsync(category);
+                await repository.SaveChangesAsync();
 
-            return category.Id;
+                return "Category added Successfully!";
+            }
+
+            return "Category already exists!";
+        }
+
+        public async Task<IEnumerable<Category>> AllAsync()
+        {
+            return repository.All<Category>();
+        }
+
+        public async Task<bool> ExistsByIdAsync(int categoryId)
+        {
+            return await repository.AllAsReadOnly<Category>().AnyAsync(c => c.Id == categoryId);
+        }
+
+        public async Task<Category> GetByIdAsync(int categoryId)
+        {
+            return await repository.AllAsReadOnly<Category>().FirstOrDefaultAsync(c => c.Id == categoryId);
+        }
+
+        public async Task<Category> GetByNameAsync(string categoryName)
+        {
+            return await repository.AllAsReadOnly<Category>().FirstOrDefaultAsync(c => c.Name == categoryName);
+        }
+
+        public async Task<string> Remove(int id)
+        {
+            if (await ExistsByIdAsync(id))
+            {
+                await repository.RemoveAsync<Category>(id);
+                await repository.SaveChangesAsync();
+                return "Category removed successfully!";
+            }
+            return "Category doesn't exists!";
         }
     }
 }
