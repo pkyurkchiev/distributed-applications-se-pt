145b81339aff7d602a4fb860de3c119b21b4dbe8
Mustafa Buhov <118231484+MustyX23@users.noreply.github.com>
2024-11-25 22:38:17 +0200

    Base Implementation of CategoryController (#22)
    * Create CategoriesController.cs
    
    Only one action is set, more to come
    
    * Update CategoriesController.cs

--- GustoHub.API/GustoHub.API/Controllers/CategoriesController.cs
+++ GustoHub.API/GustoHub.API/Controllers/CategoriesController.cs
@@ -17,10 +17,27 @@
         }
 
         [HttpPost]
-        public async Task<ActionResult<Category>> PostCategory([FromBody] Category category)
+        public async Task<IActionResult> PostCategory([FromBody] Category category)
         {
-            await categoryService.AddCategoryAsync(category);
+            string responseMessage = await categoryService.AddAsync(category);
+            return Ok(responseMessage);
+        }
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var allCategories = await categoryService.AllAsync();
+            return Ok(allCategories);
+        }
+        [HttpGet("{categoryName}")]
+        public async Task<IActionResult> GetCategoryByName(string categoryName)
+        {
+            var category = await categoryService.GetByNameAsync(categoryName);
             return Ok(category);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveCategory(int id)
+        {
+            return Ok(await categoryService.Remove(id));
         }
     }
 }
